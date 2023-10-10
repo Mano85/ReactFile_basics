32d5ab7bd09a82802206e31fab4e4a01d65656f0
Mano85 <105553749+Mano85@users.noreply.github.com>
2023-10-10 18:20:19 +0530

    react with cdn links work successfully

--- first.js
+++ first.js
@@ -1,10 +1,6 @@
 
-const mainhead=document.getElementById("main");
-console.log(mainhead);
-
-//react//
-var h1React = React.createElement("h1", {}, "HEADING CREATED using React");
-
+var h1React =React.createElement("h1", {}, "HEADING CREATED ");
 var htmlRoot = document.getElementById("root");
 var rootReact = ReactDOM.createRoot(htmlRoot);
+
 rootReact.render(h1React);
